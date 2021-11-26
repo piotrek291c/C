e1b7614ac782dc902ab1c4b37e904ea453ea6450
Piotr <Piotr@gmail.com>
2021-11-26 20:51:04 +0100

    add new template

---
+++ source/template/templateStandart_Types.h
@@ -0,0 +1,133 @@
+#ifndef STANDART_TYPES_H
+#define STANDART_TYPES_H
+
+/**********************************************************************************************************************
+ * INCLUDES SECTION
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  GLOBAL CONSTANT MACROS SECTION
+ *********************************************************************************************************************/
+
+# define MSB_FIRST        0u    /* big endian ordering bit */
+# define LSB_FIRST        1u    /* little endian ordering bit */
+
+# define HIGH_BYTE_FIRST  0u    /* big endian ordering bit */
+# define LOW_BYTE_FIRST   1u    /* little endian ordering bit */
+
+/* Boolena value */
+#ifndef TRUE
+#define TRUE            1u
+#endif
+
+#ifndef FALSE
+#define FALSE           0u
+#endif
+
+# define STD_HIGH     1u /* Physical state 5V or 3.3V */
+# define STD_LOW      0u /* Physical state 0V */
+
+# define STD_ACTIVE   1u /* Logical state active */
+# define STD_IDLE     0u /* Logical state idle */
+
+# define STD_ON       1u
+# define STD_OFF      0u
+
+/**********************************************************************************************************************
+ *  GLOBAL FUNCTION MACROS SECTION
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  GLOBAL DATA TYPES AND STRUCTURES SECTION
+ *********************************************************************************************************************/
+
+ typedef unsigned char         boolean;       /*        TRUE .. FALSE           */
+
+ typedef signed char           sint8;         /*        -128 .. +127            */
+
+ typedef unsigned char         uint8;         /*           0 .. 255             */
+
+ typedef signed short          sint16;        /*      -32768 .. +32767          */
+
+ typedef unsigned short        uint16;        /*           0 .. 65535           */
+
+ typedef signed long           sint32;        /* -2147483648 .. +2147483647     */
+
+ typedef unsigned long         uint32;        /*           0 .. 4294967295      */
+
+
+
+ typedef signed int            sint8_least;   /* At least 7 bit + 1 bit sign    */
+
+ typedef unsigned int          uint8_least;   /* At least 8 bit                 */
+
+ typedef signed int            sint16_least;  /* At least 15 bit + 1 bit sign   */
+
+ typedef unsigned int          uint16_least;  /* At least 16 bit                */
+
+ typedef signed int            sint32_least;  /* At least 31 bit + 1 bit sign   */
+
+ typedef unsigned int          uint32_least;  /* At least 32 bit                */
+
+
+#if defined(__ghs__) && !defined(__LLONG_BIT)
+
+ /*
+   The option --no_long_long for GHS compiler disables the support for the long long data type and generates compile
+   errors if this type is used. Following typedefs are omitted if --no_long_long is
+   given (__LLONG_BIT is not defined in this case).
+ */
+
+#else
+
+#define PLATFORM_SUPPORT_SINT64_UINT64
+
+ typedef signed long long      sint64;        /* -9223372036854775808 .. 9223372036854775807      */
+
+ typedef unsigned long long    uint64;        /*                    0 .. 18446744073709551615     */
+
+#endif
+
+
+
+#if defined(__ghs__) && defined(__NoFloat__)
+
+ /*
+
+   The option -fnone for GHS compiler disallows all floating-point operations and generates compile errors if such
+   operations are used by the software. This also applies to any corresponding type definition. 
+   Following typedefs are omitted if -fnone is given (__NoFloat__ is defined in this case).
+
+ */
+
+# else
+
+ typedef float                 float32;
+
+ typedef double                float64;
+
+# endif
+
+ /* This typedef has been added for OSEK compliance */
+
+#define E_OK       0U
+
+#define E_NOT_OK   1U
+
+#define E_PENDING  10U
+
+typedef uint8 Std_ReturnType;
+
+/**********************************************************************************************************************
+ *  GLOBAL DATA PROTOTYPES SECTION
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  GLOBAL FUNCTION PROTOTYPES SECTION
+ *********************************************************************************************************************/
+
+#endif  /* STANDART_TYPES_H */
+
+/**********************************************************************************************************************
+ *  END OF FILE: Standart_Types.h
+ *********************************************************************************************************************/
